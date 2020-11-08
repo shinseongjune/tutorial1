d5ac615c73e52d92150ee356d7008fc1c29f82b8
shinseongjune <shinseongjune.dev@gmail.com>
2020-11-08 19:29:10 +0900

    start

---
+++ tutorial2-android/src/com/sodaus/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.sodaus.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.sodaus.game.Tutorial;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Tutorial(), config);
+	}
+}
